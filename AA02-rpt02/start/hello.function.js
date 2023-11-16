00522b5901c3e3f11d8cd85eff1e9120cf9212cd
spaceko126 <56930395+spaceko126@users.noreply.github.com>
2023-11-16 14:42:15 +0900

    Add files via upload

---
+++ AA02-rpt02/start/hello.function.js
@@ -0,0 +1,5 @@
+function hello(what){
+    console.log("Hello"+what+" !");
+}
+hello("aa02");
+hello("redwoods, 고서진");
---
+++ AA02-rpt02/start/hello_call_module.js
@@ -0,0 +1,4 @@
+var olleh=require('./hello_user_module');
+
+olleh("Node");
+olleh("aa02");
